0d9dec39262883bdcb617f582f9f018e1d3d1cc1
Robbie  H <ecofemale@hotmail.com>
2019-03-11 20:45:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  !position_taken?(board, index) && 
+  !position_taken?(board, index) && index.to_i.between?(0, 8)
 end
 
      def position_taken(board, index)
