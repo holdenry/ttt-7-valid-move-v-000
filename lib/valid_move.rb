873f00e4e253b7b621faf3d6aa65a46bc52b71a0
Robbie  H <ecofemale@hotmail.com>
2019-03-11 20:34:15 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   #position_taken?(board, index)
 end
 
-     def position_taken?(board, index)
+     def valid_move?(board, index)
       if board[index] == " " || board[index] == "" || board[index] == nil || board[index] 
         return false
       
